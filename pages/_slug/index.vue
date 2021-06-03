4c4efde3f94c5627d04386e027c4b20baeb519aa
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-06-03 23:01:39 +0600

    ~ content.md->lower case: boom magic.

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
     <h1>something</h1>
+    <h1>{{ article.title }}</h1>
   </div>
 </template>
 
 <script>
   export default {
     async asyncData({ $content, params }) {
-      const article = await $content('art', params.slug)
+      const article = await $content( params.slug )
         .fetch()
-      // the article being returned is an array of object. i need a single object.
-      console.log(article)
       return {
         article
       }
