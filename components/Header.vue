9cd7412da5164c43c239f584760538ae3d9b2a87
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-06-04 11:35:44 +0600

    ++ header

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="bg-red-500 sticky w-10 h-full">
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
