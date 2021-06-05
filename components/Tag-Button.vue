dcf600057cd191bf0e9d90f2c6f6cc7e2e4b8b45
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-06-05 17:46:34 +0600

    tag returns to home

--- components/Tag-Button.vue
+++ components/Tag-Button.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-2 flex flex-row justify-center">
-    <button v-for="(tag, index) of article.tags" :key="index" class="mx-2 p-1 rounded-xl bg-blue-400 shadow-lg">
+    <button v-for="(tag, index) of article.tags" :key="index" @click="returnToThisTag(tag)" class="mx-2 p-1 rounded-xl bg-blue-400 shadow-lg">
       {{ tag }}
     </button>
   </div>
@@ -12,6 +12,12 @@
       article: {
         type: Object,
         default: () => null
+      }
+    },
+    methods: {
+      returnToThisTag(tag) {
+        this.$router.push('/');
+        console.log(tag)
       }
     }
   }
