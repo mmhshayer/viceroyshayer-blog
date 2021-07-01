343b5b61baf5fed5bf04e2626adeb2618f0f941b
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-07-01 22:35:16 +0600

    surr post

--- components/Surr-Post.vue
+++ components/Surr-Post.vue
@@ -1,25 +1,25 @@
 <template>
   <nav class="py-2 flex flex-row justify-between">
-    <div class="pl-5 text-left block hover:shadow-md w-1/2 h-1/2 rounded-xl
+    <div v-if="prev" class="pl-5 text-left block hover:shadow-md w-1/2 h-1/2 rounded-xl
     bg-gradient-to-r from-green-400 hover:from-pink-500
     ">
-      <NuxtLink v-if="prev" :to="{ name: 'slug', params: { slug: prev.slug } }">
+      <NuxtLink :to="{ name: 'slug', params: { slug: prev.slug } }">
         <span>« PREV PAGE</span>
         <br>
         {{ prev.title }}
       </NuxtLink>
-      <span v-else>&nbsp;</span>
     </div>
-    <div class="pr-5 text-right block hover:shadow-md w-1/2 h-1/2 rounded-xl
+    <span v-else>&nbsp;</span>
+    <div v-if="next" class="pr-5 text-right block hover:shadow-md w-1/2 h-1/2 rounded-xl
     bg-gradient-to-l from-green-400 hover:from-pink-500
     ">
-      <NuxtLink v-if="next" :to="{ name: 'slug', params: { slug: next.slug } }">
+      <NuxtLink :to="{ name: 'slug', params: { slug: next.slug } }">
         <span >NEXT PAGE »</span>
         <br>
         {{ next.title }}
       </NuxtLink>
-      <span v-else>&nbsp;</span>
     </div>
+    <span v-else>&nbsp;</span>
   </nav>
 </template>
 
